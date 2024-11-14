79aae9e3916d58e1cf05e437e3a04892be37c815
System Administrator <root@xishnaht108.local>
2024-11-15 03:42:31 +0700

    youtube

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -6,6 +6,27 @@ const mongoose = require("mongoose");
 
 const router = express.Router();
 
+// Route để lấy danh sách tất cả các phim
+router.get("/", async (req, res) => {
+  try {
+    const movies = await Movie.find(); // Truy vấn tất cả phim trong database
+    res.json(movies); // Trả về danh sách phim dưới dạng JSON
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const movie = await Movie.findById(req.params.id); // Thay đổi tùy theo cách lấy dữ liệu của bạn
+    if (!movie) return res.status(404).json({ message: "Movie not found" });
+    res.json({ movie });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
 // Route to get movie details by ID, including reviews with user details
 router.get("/:id/reviews", async (req, res) => {
   try {
