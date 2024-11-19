46cbbd35ac930dd63240d4117338d251beb2dd6b
System Administrator <root@xishnaht108.local>
2024-11-20 00:32:30 +0700

    feat: movie module

---
+++ routes/screeningRoutes.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const router = express.Router()
+const mongoose = require('mongoose')
+const Screening = require('../models/screening')
+
+// Route to get screenings by both date and movie_id
+
+router.get('/', async (req, res) => {
+  try {
+    const { date, movie_id } = req.query
+
+    console.log('Received date:', date)
+    console.log('Received movie_id:', movie_id)
+
+
+    // Validate that both parameters are provided
+    if (!date || !movie_id) {
+      return res.status(400).json({ error: 'Both date and movie_id query parameters are required.' })
+    }
+
+    // Validate `movie_id` format
+    if (!mongoose.Types.ObjectId.isValid(movie_id)) {
+      return res.status(400).json({ error: 'Invalid movie_id format. Must be a 24-character hex string.' })
+    }
+
+    // Parse the date and create a range for the full day
+    const startOfDay = new Date(date)
+    const endOfDay = new Date(date)
+    endOfDay.setHours(23, 59, 59, 999)
+
+    // Query the screenings collection
+    const screenings = await Screening.find({
+      movie_id, // Ensure proper ObjectId casting
+      screening_time: {
+        $gte: startOfDay,
+        $lte: endOfDay,
+      },
+    })
+
+    res.status(200).json(screenings)
+  } catch (error) {
+    console.error('Error fetching screenings:', error)
+    res.status(500).json({ error: 'Internal server error' })
+  }
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,32 +1,35 @@
-const express = require('express');
-const cors = require('cors');
-const connectDB = require('./config/db');
-const movieRoutes = require('./routes/movieRoutes');
-const authRoutes = require('./routes/authRoutes');
+const express = require('express')
+const cors = require('cors')
+const connectDB = require('./config/db')
+const movieRoutes = require('./routes/movieRoutes')
+const authRoutes = require('./routes/authRoutes')
 const productRoutes = require('./routes/productRoutes')
+const screeningRoutes = require("./routes/screeningRoutes")
 
-const app = express();
+const app = express()
 
-connectDB();
+connectDB()
 
-app.use(cors());
+app.use(cors())
 
 // Middleware để parse body request
-app.use(express.json());
+app.use(express.json())
 
 // Middleware cơ bản để kiểm tra
 app.get('/', (req, res) => {
-    res.send('<h1>Welcome to the Cinema app!</h1>');
-});
+  res.send('<h1>Welcome to the Cinema app!</h1>')
+})
 
-app.use('/movies', movieRoutes);
+app.use('/movies', movieRoutes)
 
 app.use('/auth', authRoutes)
 
 app.use('/products', productRoutes)
 
+app.use("/screenings", screeningRoutes)
+
 app.listen(3001, () => {
-  console.log("Server is running on 3001");
-});
+  console.log("Server is running on 3001")
+})
 
 module.exports = app
